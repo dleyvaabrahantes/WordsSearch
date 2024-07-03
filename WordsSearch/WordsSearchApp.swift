21ba099ce5bc10e929ff8b5f4c8bc5519efa035d
David Leyva <dleyvaabrahantes@gmail.com>
2024-07-03 11:15:38 -0400

    Initial Commit

---
+++ WordsSearch/WordsSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsSearchApp.swift
+//  WordsSearch
+//
+//  Created by David on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WordsSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
